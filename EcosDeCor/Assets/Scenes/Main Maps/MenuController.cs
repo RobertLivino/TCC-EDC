a6fb159530eb184aa2f1a739795a9fbc7c3366bd
Rodrigo <rodrygowork@gmail.com>
2024-10-23 14:45:45 -0300

    MenuDefinitivoSemMusica

--- EcosDeCor/Assets/Scenes/Main Maps/MenuController.cs
+++ EcosDeCor/Assets/Scenes/Main Maps/MenuController.cs
@@ -3,12 +3,19 @@ using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
-    public GameObject imgComandos; // arraste sua imagem para este campo no Inspector
+    public GameObject imgComandos; // 
 
     // Função que será chamada ao clicar no botão
     public void MostrarComandos()
     {
-        // Alterna entre ativar e desativar a imagem
-        imgComandos.SetActive(!imgComandos.activeSelf);
+        
+        imgComandos.SetActive(true);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            imgComandos.SetActive(false);
+        }
     }
 }
