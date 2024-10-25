25c26a563d0b0b148334887d1f490cf01ddcd156
Robert Livino <robertlivino08@gmail.com>
2024-10-25 16:33:56 -0300

    commit inimigo

--- EcosDeCor/Assets/Scripts/EnemyCrab.cs
+++ EcosDeCor/Assets/Scripts/EnemyCrab.cs
@@ -9,6 +9,8 @@ public class EnemyCrab : MonoBehaviour
     public MapaController mapaController;
     public HealthBar healthBar;
     public Transform visao;
+    public NavMeshAgent agent;
+    public LayerMask playerLayerMask;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +27,13 @@ public class EnemyCrab : MonoBehaviour
             mapaController.healthHealt = true;
             mapaController.healthHealtValue = 1f;
             Destroy(gameObject);
+        }
+        //Debug.DrawRay(visao.transform.position, visao.transform.forward);
+        Ray ray = new Ray(visao.transform.position, visao.transform.forward);
+        Debug.DrawRay(ray.origin, ray.direction);
+        if (Physics.Raycast(ray, 0, playerLayerMask))
+        {
+            Debug.Log("Player");
         }
     }
     private void OnTriggerEnter(Collider other)
